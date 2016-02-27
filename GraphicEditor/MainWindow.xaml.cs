6c7b4e272f322c3634cfdbfe9670bba1644fbe07
ptaszysko <weronika.sawicka.9@gmail.com>
2016-02-27 23:24:51 +0100

    Dziala: wyswietlanie, wczytywanie wartosci, edycja Poprawic: ponowne wczytywanie edytowanego obrazka

--- GraphicEditor/MainWindow.xaml.cs
+++ GraphicEditor/MainWindow.xaml.cs
@@ -10,7 +10,8 @@ namespace GraphicEditor
     public partial class MainWindow : Window
     {
         private Bitmap imageDisplayed = null;
-        private Color? color = null;
+        private Color color;
+        System.Windows.Point point;
 
         public MainWindow()
         {
@@ -19,7 +20,7 @@ namespace GraphicEditor
 
         private void HandleButton_OpenFile(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
             openFileDialog.FilterIndex = 1;
             //openFileDialog.InitialDirectory = @"C:\Users\mloda\Desktop";
             openFileDialog.Filter = "Image File|*.jpg; *.jpeg; *.tiff; *.gif; *.png;";
@@ -31,14 +32,31 @@ namespace GraphicEditor
             }
         }
 
-        private void HandleImage_MouseUp(object sender, MouseEventArgs e)
+        private void HandleImage_MouseUp(object sender, MouseButtonEventArgs e)
         {
+            RBox.Text = "";
+            GBox.Text = "";
+            BBox.Text = "";
+
             if (imageDisplayed != null)
             {
-                Console.WriteLine("Inside!");
-                //imageDisplayed.GetPixel();
+                point = Mouse.GetPosition(image);
+                positionX.Content = (int)point.X;
+                positionY.Content = (int)point.Y;
+                color = imageDisplayed.GetPixel((int)point.X, (int)point.Y);
+                RBox.Text = color.R.ToString();
+                GBox.Text = color.G.ToString();
+                BBox.Text = color.B.ToString();
             }
+        }
 
+        private void HandleButton_Save(object sender, RoutedEventArgs e)
+        {
+            color = Color.FromArgb(Int32.Parse(RBox.Text), Int32.Parse(GBox.Text), Int32.Parse(BBox.Text));
+            imageDisplayed.SetPixel((int)point.X, (int)point.Y, Color.FromArgb(color.A, color.R, color.G, color.B));
+            image.Source = null;
+            // TODO: repaint image to show changes
+            //image.Source = (BitmapImage) image;
         }
     }
 }
