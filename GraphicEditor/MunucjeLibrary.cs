743007bf34da67dbd7ef331a3b8af833ed23a17b
wioleta <wioleta0294@gmail.com>
2016-05-09 15:22:56 +0200

    poprawki2

--- GraphicEditor/MunucjeLibrary.cs
+++ GraphicEditor/MunucjeLibrary.cs
@@ -91,15 +91,22 @@ namespace GraphicEditor
                     }
 
 
-                    
-                    if (przeciecia == 1 && przeciecia2==1)
+
+
+                    if (przeciecia == 1 && przeciecia2 == 1 && DodatkowaMaska(renderedImage, i, j))
                     {
                         map[i, j] = 1; /// zakonczenie
                     }
-                    else if(przeciecia == 3 && przeciecia2 == 3)
+                    //if (przeciecia == 1 && przeciecia2 == 1)
+                    //{
+                    //    map[i, j] = 1; /// zakonczenie
+                    //}
+                    else if (przeciecia == 3 && przeciecia2 == 3)
                     {
                         map[i, j] = 2; // rozwidlenie
                     }
+
+               
 
                     
                 }
@@ -108,6 +115,137 @@ namespace GraphicEditor
             Zaznaczpunkty(renderedImage, map);
 
             return renderedImage;
+
+        }
+
+        //public int[,] UsunDuplikaty(int[,] map)
+        //{
+        //    for (int i = 0; i < map.GetLength(0); i++)
+        //    {
+        //        for (int k = 0; k < map.GetLength(1); k++)
+        //        {
+        //            if (map[i, k] == 2)
+        //            {
+        //                bool endregion = true;
+
+        //                var indeksRemove = new List<Tuple<int, int>>();
+
+
+
+        //                while (endregion)
+        //                {
+        //                    bool end = true;
+
+        //                    int count = 0;
+        //                    int ii = i;
+        //                    int kk = k;
+
+        //                    while (end)
+        //                    {
+        //                        if (map[ii, kk] == 0)
+        //                        {
+        //                            end = false;
+        //                            kk++;
+        //                            ii = i;
+        //                        }
+        //                        else
+        //                        {
+        //                            ii++;
+        //                            count++;
+        //                        }
+
+        //                        if (map[ii, kk] == 0)
+        //                        {
+        //                            endregion = false;
+        //                            end = false;
+        //                        }
+
+        //                    }
+        //                }
+
+        //            }
+        //        }
+        //    }
+
+        //}
+
+        //public Tuple<int,int> PoliczDlugosc(int[,] map, int x, int y)
+        //{
+
+        //    int wys = 0;
+        //    int szer = 0;
+
+        //    int i = 1;
+        //    bool end = false;
+
+        //    while (end)
+        //    {
+        //        if (map[x + i, y + i] == 2 &&
+        //           map[x - i, y - i] == 2 &&
+        //           map[x + i, y - i] == 2 &&
+        //           map[x - i, y + i] == 2 &&
+
+        //           map[x, y - i] == 2 &&
+        //           map[x, y + i] == 2 &&
+        //           map[x + i, y] == 2 &&
+        //           map[x - i, y] == 2
+        //          )
+        //        {
+
+        //            i++;
+        //            wys++; szer++;
+        //        }
+        //        else
+        //        {
+        //            end = false;
+        //            return Tuple.Create(wys, szer);
+        //        }
+
+        //    }
+        //}
+
+        
+
+        public bool DodatkowaMaska(Bitmap renderedImage, int x, int y)
+        {
+      
+            int przeciecia = 0;
+
+            Color colorPunktu = renderedImage.GetPixel(x, y);
+
+            for (int u = -1; u <= 1; u++)
+            {
+                for (int k = -1; k <= 1; k++)
+                {
+
+                    
+                    if (u == 0 && k == 0)
+                        continue;
+
+                    if ((u != -1&& k != -1) && (u != 1 && k != 1))
+                    {
+                        continue;
+                    }
+
+                    Color color = renderedImage.GetPixel(x + u, y + k);
+
+                    ///Color color = renderedImage.GetPixel(i + u, j + k);
+
+                    //Color color2 = renderedImage.GetPixel((i + u +1 ) % 8 , ( j + k +1 )  % 8 );
+
+                    if (color.R == 0 && colorPunktu.R == 0)
+                    {
+                        przeciecia++;
+                    }
+
+                }
+            }
+
+            if (przeciecia < 3)
+                return true;
+            else
+                return false;
+
 
         }
 
